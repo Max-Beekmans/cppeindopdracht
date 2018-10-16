e2027035f5074cea78ed391c6f95cbc6c0d4b85e
Max Beekmans <mbeekman1@avans.nl>
2018-10-16 14:51:11 +0200

    added test framework to project

---
+++ test/StringTest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by MaxBe on 10/16/2018.
+//
+
+#include "catch2/catch.hpp"
+#include "../nostd/String.h"
+
+TEST_CASE("Compare two strings", "[single-file]") {
+    nostd::String a("String One");
+    nostd::String b("String Two");
+    REQUIRE(a == b);
+}
