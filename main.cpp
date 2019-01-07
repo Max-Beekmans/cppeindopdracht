f71e00f80189cbf79bf3e0003644d4602110112c
Max Beekmans <mbeekman1@avans.nl>
2019-01-07 10:48:29 +0100

    I don't know

--- main.cpp
+++ main.cpp
@@ -3,13 +3,7 @@
 #include "nostd/String.h"
 #include "nostd/Array.h"
 #include "nostd/Random.h"
-#include "Game.h"
 
 int main() {
-    Game g {};
-    g.Start();
-    g.Stop();
-    char c[65];
-    std::cin >> c;
     return 0;
 }
