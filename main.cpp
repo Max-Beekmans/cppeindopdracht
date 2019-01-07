e6afeb1303581e23aeac385df9d027cd0d9f1471
Max Beekmans <mbeekman1@avans.nl>
2019-01-07 11:18:29 +0100

    temp commit cause no power

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 #include "nostd/String.h"
 #include "nostd/Array.h"
 #include "nostd/Random.h"
+#include "nostd/IOHandler.h"
 
 int main() {
+    nostd::IOHandler io {};
+    io.Print("I like trains");
+    nostd::String input = io.GetString();
+    io.Print(input);
     return 0;
 }
